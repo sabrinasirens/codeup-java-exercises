c66562cd98ee1ecf32349ff12343e085139cb60e
Sabrina Burch <sabrinaburch@SabrinaurchsMBP.localdomain>
2023-12-06 12:35:22 -0600

    Completed Variable Exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,32 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int myFavoriteNumber = 2;
+        String myString = "Testing";
+        double shortPi = 3.14;
+        float myNumber = (float) shortPi;
         System.out.print("Hello");
-        System.out.print(" World!");
+        System.out.println(" World!");
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        System.out.println(myNumber);
+
+        /*int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+         */
+
+       /* String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+        int three = (int) "three";
+        System.out.println(three);
+        */
+
+        short x = 8;
+        short y = 20000;
+        x *= y;
+        //y -= x;
+        System.out.println(x);
+        //System.out.println(y);
     }
 }
